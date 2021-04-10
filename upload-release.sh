abd654e76c231f41ada9f91f7a9fd30b1a370f95
thejoin95 <miki.lombi@gmail.com>
2021-04-10 22:09:50 +0200

    fix bash script

--- upload-release.sh
+++ upload-release.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -l
 
-# if $TWINE_USERNAME == "" || $TWINE_PASSWORD == ""
-# then
-#     echo "No twine username"
-#     return -1
-# fi
+if $TWINE_USERNAME == "" || $TWINE_PASSWORD == ""
+then
+    echo "No twine info in the environment variables"
+    return -1
+fi
 
-# exec python setup.py sdist bdist_wheel
-# echo "Build finished"
+python setup.py sdist bdist_wheel
+echo "Build finished"
 
 # TODO: check if dist & build directory are existing and also with the correct files
 
 echo "Twine init"
-exec twine upload dist/*
+twine upload dist/*
 echo "Twine ended"
