e68eeabe019f9e5e3d49fa2cb08afa8958a26a75
thejoin95 <miki.lombi@gmail.com>
2021-04-10 21:03:28 +0200

    add workflow in upload-release.sh

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,7 @@ FROM python:3.9-slim
 WORKDIR /
 COPY upload-release.sh .
 
+RUN pip install twine && pip install setuptools
+
 RUN chmod +x upload-release.sh
 ENTRYPOINT [ "upload-release.sh" ]
--- upload-release.sh
+++ upload-release.sh
@@ -1,1 +1,13 @@
-# upload script
+#!/usr/bin/bash
+
+if TWINE_USERNAME == "" || TWINE_PASSWORD == ""
+then
+    echo "No twine username"
+    exit()
+fi
+
+exec python .\setup.py sdist bdist_wheel
+
+# TODO: check if dist & build directory are existing and also with the correct files
+
+exec twine -u TWINE_USERNAME -p TWINE_PASSWORD upload dist/*
