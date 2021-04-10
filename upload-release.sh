08cb225aa4cd1987c36e7cdbc3640ea62f86584e
thejoin95 <miki.lombi@gmail.com>
2021-04-10 21:49:55 +0200

    fix environment variable in shell file

--- upload-release.sh
+++ upload-release.sh
@@ -1,13 +1,16 @@
 #!/bin/sh -l
 
-if TWINE_USERNAME == "" || TWINE_PASSWORD == ""
+if $TWINE_USERNAME == "" || $TWINE_PASSWORD == ""
 then
     echo "No twine username"
     return -1
 fi
 
 exec python setup.py sdist bdist_wheel
+echo "Build finished"
 
 # TODO: check if dist & build directory are existing and also with the correct files
 
-exec twine -u TWINE_USERNAME -p TWINE_PASSWORD upload dist/*
+echo "Twine init"
+exec twine -u $TWINE_USERNAME -p $TWINE_PASSWORD upload dist/*
+echo "Twine ended"
